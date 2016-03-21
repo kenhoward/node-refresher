952b769bd3e05c1a8e27d68f4ef65f4981218fad
Kenny <ken.chad.howard@gmail.com>
2016-03-21 17:00:10 -0600

    getting data

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
+//Using Body Parser for our POST requests
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -9,9 +11,19 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+//Telling bodyParser what we need it to: JSONify the content
+app.use(bodyParser.json());
+//Helps with encoding
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+  .post(function(req,res) {
+    var book = new Book(req.body);
+    console.log(book)
+    res.send(book);
+  })
   .get(function(req,res) {
     //Adding a filtering option [Module: 2 - Getting Data]
     var query = {};
@@ -29,18 +41,18 @@ bookRouter.route('/Books')
 
 //Utilizing ID to return single item [Module: 2 - Getting a Single Item]
 bookRouter.route('/Books/:bookId')
-.get(function(req,res) {
-  //Adding a filtering option [Module: 2 - Getting Data]
-  var query = {};
+  .get(function(req,res) {
+    //Adding a filtering option [Module: 2 - Getting Data]
+    var query = {};
 
-  if(req.query.genre) {
-    query.genre = req.query.genre; //Only allows us to filter on that one piece
-  }
-  Book.findById(req.params.bookId, function(err,book) {
-    if(err)
-      res.status(500).send(err);
-    else
-      res.json(book);
+    if(req.query.genre) {
+      query.genre = req.query.genre; //Only allows us to filter on that one piece
+    }
+    Book.findById(req.params.bookId, function(err,book) {
+      if(err)
+        res.status(500).send(err);
+      else
+        res.json(book);
   });
 });
 
